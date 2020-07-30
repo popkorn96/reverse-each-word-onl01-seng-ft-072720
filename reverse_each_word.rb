5b8f9c73c03a65e3b6b0035037b4077735cef79e
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-30 23:16:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,9 @@
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
-  array.each do |string|
+  array.collect do |string|
     test_array << string.reverse
   end
   test_array.join(" ")
   end
   
-  def reverse_each_word(string)
-    array1 = string.split(" ")
-    return_array = []
-    array.collect do |string|
-      return_array << string.reverse
-    end
-    return_array.join(" ")
-  end
